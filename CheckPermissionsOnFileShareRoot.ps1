86647b2430764b9361b7fe1434c4bdc256d2a99d
bed428 <72625711+bed428@users.noreply.github.com>
2024-09-11 09:50:32 -0600

    Update CheckPermissionsOnFileShareRoot.ps1

--- CheckPermissionsOnFileShareRoot.ps1
+++ CheckPermissionsOnFileShareRoot.ps1
@@ -1,23 +1,36 @@
-
 $Servername = "servername"
 $shares = net view \\$Servername | ForEach-Object { $_.Trim() } | Where-Object { $_ -match 'Disk' } | ForEach-Object {
 	$_ -split '\s{2,}' | Select-Object -First 1
 }
 
-
 $Allowed = @()
 $Denied = @()
+$TestPathSuccess = @()
+
 foreach ($share in $shares) {
 	$sharePath = "\\$Servername\$share"
 	try {
     	Test-Path $sharePath -ErrorAction Stop | Out-Null
-    	$Allowed += $sharePath    
+    	$TestPathSuccess += $sharePath    
 	} catch {
     	$Denied += $sharePath
 	}
 }
 
-Write-Host -ForegroundColor Green "##### ALLOWED #####"
+foreach($success in $TestPathSuccess){
+	try {
+    	Get-ChildItem $sharePath -ErrorAction Stop | Select-Object -first 5 | Out-Null
+    	$Allowed += $sharePath
+	} catch {
+    	$Denied += $sharePath
+
+}
+Write-Host -ForegroundColor Green "#####ALLOWED#####"
 foreach($allow in $Allowed){Write-Host -ForegroundColor Green $allow}
-Write-Host -ForegroundColor Red `n "##### DENIED #####"
+Write-Host -ForegroundColor Red `n "#####Denied#####"
 foreach($deny in $denied){Write-Host -ForegroundColor Red $deny}
+
+
+
+
+
